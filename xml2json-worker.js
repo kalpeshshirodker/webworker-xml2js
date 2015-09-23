4b431afd7902f41c531914b57da15f297887913d
kalpeshshirodker <kalpeshshirodker@users.noreply.github.com>
2015-09-23 15:35:34 +0530

    Create xml2json-worker.js
    Parse an XML String to JSON object using webworkers.
    Depends on : sax.js, assure.js, xml2json-streaming.js

---
+++ xml2json-worker.js
@@ -0,0 +1,30 @@
+importScripts('../scripts/sax.js');
+importScripts('../scripts/assure.js');
+importScripts('xml2json-streaming.js');
+self.onmessage = function(e){
+	var serializedData = e.data;	
+	var p, x2jc;
+	
+	//Sanitize cdata
+	serializedData = serializedData.replace(/<!--\[/g, "<![");
+	serializedData = serializedData.replace(/\]\]-->/g, "]]>");
+	
+	x2jc = new xml2jsconverter();
+	p = x2jc.parse(serializedData);
+	
+	//TODO:handle progress information
+	p.done(function(data){		
+		self.postMessage({
+			eventType : data.eventType,
+			obj : data.obj,
+			status : data.status
+		});
+	});
+	p.fail(function(data){
+		self.postMessage({
+			obj : data.obj,
+			status : data.status
+		});
+	});
+}
+
